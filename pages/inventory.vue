cb5064c8f9c5f2393afd2bb7bd45983f502e0309
madhavcodez <madhavcbusiness@gmail.com>
2024-10-24 16:16:21 -0500

    Update inventory.vue

--- pages/inventory.vue
+++ pages/inventory.vue
@@ -1,19 +1,30 @@
 <template>
-    <div>
-      <main>
-        <h1>Inventory Page</h1>
-        <p>Welcome to the inventory page. Here you can manage your inventory items.</p>
-      </main>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    // You can add component-specific logic here if needed
+  <div>
+    <h1>Inventory</h1>
+    <ul>
+      <li v-for="item in inventory" :key="item.id">
+        <p>{{ item.name }}</p>
+        <BarcodeGenerator :upcCode="item.upcCode" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import BarcodeGenerator from '~/components/BarcodeGenerator.vue';
+import { useRuntimeConfig } from '#app';
+
+const inventory = ref([]);
+const config = useRuntimeConfig();
+
+onMounted(async () => {
+  try {
+    inventory.value = await $fetch('/api/inventory/', {
+      baseURL: config.public.apiBase,
+    });
+  } catch (error) {
+    console.error('Fetch error:', error);
   }
-  </script>
-  
-  <style scoped>
-  /* Add your styles here or import a separate CSS file */
-  /*@import 'styles.css'; /* Assuming styles.css is in the assets directory */
-  </style>
+});
+</script>
