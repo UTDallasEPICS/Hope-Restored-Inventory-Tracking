e31382feea9fdde694822af2c846b3e63932d416
madhavcodez <madhavcbusiness@gmail.com>
2024-10-24 16:07:42 -0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+
+  // Adding the Axios module
+  modules: ['@nuxtjs/axios'],
+
+  // Configuring Axios
+  axios: {
+    baseURL: 'http://localhost:8000', // Replace with your backend URL
+  },
+});
