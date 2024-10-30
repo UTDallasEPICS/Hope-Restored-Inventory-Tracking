5fb27e3564db5ff9a92814586de21a9eca729291
Hillary Wong <wong.hillary@yahoo.com>
2024-10-30 12:11:00 -0500

    edited HeaderComponent and reverted some files

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -1,60 +1,60 @@
-<!-- components/HeaderComponent.vue -->
+
+  <!-- components/HeaderComponent.vue -->
 <template>
-    <header>
-      <nav class="sidebar">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrkEWxs5zaMTeYj-qwtpxMkFhJtT4yieFiuQ&s" style="width:170px; height:auto;">
-          <ul>
-          <li><nuxt-link to="/">Home</nuxt-link></li>
-          <li><nuxt-link to="/inventory">Inventory</nuxt-link></li>
-          <li><nuxt-link to="/scanner">Scanner</nuxt-link></li>
-        </ul>
-      </nav>
-    </header>
-  </template>
-  
-  <script>
-  export default {
-    name: 'HeaderComponent',
-  };
-  </script>
-  
-  <style scoped>
-  /* Add styles for your header here */
-    @import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Figtree:ital,wght@0,300..900;1,300..900&family=Quicksand:wght@600&display=swap');
+  <header>
+    <nav class="navbar">
+        <!-- <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrkEWxs5zaMTeYj-qwtpxMkFhJtT4yieFiuQ&s" style="width:170px; height:auto;"> -->
+        <ul>
+        <li class="title">Hope Restored Missions</li>
+        <li><nuxt-link to="/">Home</nuxt-link></li>
+        <li><nuxt-link to="/inventory">Inventory</nuxt-link></li>
+        <li><nuxt-link to="/scanner">Scanner</nuxt-link></li>
+      </ul>
+      <hr>
+    </nav>
+  </header>
+</template>
 
-    nav {
-    background: #222222;
-    padding: 1rem;
-    }
-  
-  
-    ul {
-    list-style: none;
-    padding: 0; /* Remove default padding */
-    }
+<script>
+export default {
+  name: 'HeaderComponent',
+};
+</script>
 
-    li {
-    margin-bottom: 1rem; /* Space between items */
-    text-align: center;
-    font-family: "Figtree";
-    }
-  
-    a {
-    color: white;
-    text-decoration: none;
-    }
+<style scoped>
+/* Add styles for your header here */
+  @import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Figtree:ital,wght@0,300..900;1,300..900&family=Quicksand:wght@600&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Figtree:ital,wght@0,300..900;1,300..900&family=Oswald:wght@200..700&family=Quicksand:wght@600&display=swap');
+  nav {
+  padding: 1rem;
+  }
 
-    img {
-        border-radius: 50%;
-    }
-  
-    .sidebar {
-    background: #333333;
-    padding: 1rem;
-    width: 170px; /* Adjust width as needed */
-    position: fixed; /* Fix the sidebar in place */
-    height: 100%; /* Full height */
-    }
 
-  </style>
-  
+  ul {
+  list-style: none;
+  padding:0%;
+  /* Remove default padding */
+  }
+
+  li {
+  display: inline;
+  margin-left: 10px;
+  text-align: left;
+  font-family: "Oswald";
+  padding-bottom: 0%;
+  font-size: 20px;
+  }
+
+  a {
+  color: #878787;
+  text-decoration: none;
+  }
+
+  .title {
+    font-family: 'Oswald';
+    font-size: 30px;
+    margin-left:0%;
+    margin-right: 60px;
+    color: #878787;
+  }
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 export default defineNuxtConfig({
-
   runtimeConfig: {
     public: {
       apiBase: 'http://localhost:8000', // Replace with your backend API URL
     },
   },
+
+  compatibilityDate: '2024-10-30',
 });
