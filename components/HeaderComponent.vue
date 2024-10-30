5df95509f7f76a1b511052326a2a19ec6f78658a
Hillary Wong <wong.hillary@yahoo.com>
2024-10-30 16:56:20 -0500

    add plugins folder and header edits

--- components/HeaderComponent.vue
+++ components/HeaderComponent.vue
@@ -1,13 +1,16 @@
-<!-- components/HeaderComponent.vue -->
-<template>
+
+  <!-- components/HeaderComponent.vue -->
+  <template>
     <header>
-      <nav class="sidebar">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrkEWxs5zaMTeYj-qwtpxMkFhJtT4yieFiuQ&s" style="width:170px; height:auto;">
+      <nav class="navbar">
+          <!-- <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrkEWxs5zaMTeYj-qwtpxMkFhJtT4yieFiuQ&s" style="width:170px; height:auto;"> -->
           <ul>
+          <li class="title">Hope Restored Missions</li>
           <li><nuxt-link to="/">Home</nuxt-link></li>
           <li><nuxt-link to="/inventory">Inventory</nuxt-link></li>
           <li><nuxt-link to="/scanner">Scanner</nuxt-link></li>
         </ul>
+        <hr>
       </nav>
     </header>
   </template>
@@ -21,40 +24,38 @@
   <style scoped>
   /* Add styles for your header here */
     @import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Figtree:ital,wght@0,300..900;1,300..900&family=Quicksand:wght@600&display=swap');
-
+    @import url('https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Figtree:ital,wght@0,300..900;1,300..900&family=Oswald:wght@200..700&family=Quicksand:wght@600&display=swap');
     nav {
-    background: #222222;
     padding: 1rem;
     }
   
   
     ul {
     list-style: none;
-    padding: 0; /* Remove default padding */
+    padding:0%;
+    /* Remove default padding */
     }
-
+  
     li {
-    margin-bottom: 1rem; /* Space between items */
-    text-align: center;
-    font-family: "Figtree";
+    display: inline;
+    margin-left: 10px;
+    text-align: left;
+    font-family: "Oswald";
+    padding-bottom: 0%;
+    font-size: 20px;
     }
   
     a {
-    color: white;
+    color: #878787;
     text-decoration: none;
     }
-
-    img {
-        border-radius: 50%;
-    }
   
-    .sidebar {
-    background: #333333;
-    padding: 1rem;
-    width: 170px; /* Adjust width as needed */
-    position: fixed; /* Fix the sidebar in place */
-    height: 100%; /* Full height */
+    .title {
+      font-family: 'Oswald';
+      font-size: 30px;
+      margin-left:0%;
+      margin-right: 60px;
+      color: #878787;
     }
-
   </style>
   
